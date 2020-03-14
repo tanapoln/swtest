318769040e59b50b4c24a5fef6027b64eb3c3eb2
Tanapol Nearunchorn <tanapol.sh@gmail.com>
2020-03-15 00:53:52 +0700

    add rest template

--- src/test/java/com/example/swtest/MockExampleTest.java
+++ src/test/java/com/example/swtest/MockExampleTest.java
@@ -6,6 +6,7 @@ import org.jmock.integration.junit4.JMock;
 import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collection;
 
@@ -14,7 +15,14 @@ public class MockExampleTest {
     private Mockery mock = new JUnit4Mockery();
 
     @Test
-    public void xxx() {
+    public void testWebHook() {
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.postForObject("https://enx8m1v7ntjv.x.pipedream.net/sharp", "Hi from sharp", String.class);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testMock() {
         Collection c = mock.mock(Collection.class);
         mock.checking(new Expectations() {{
             oneOf(c).isEmpty();
